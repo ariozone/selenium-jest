67932ddd980e74efc59ea89f43c7d97925df5272
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-29 18:33:34 -0700

    Setup test to create driver and open page before tests and teardown and close it after all tests.

--- rsvp/tests/invitees.test.js
+++ rsvp/tests/invitees.test.js
@@ -4,11 +4,22 @@ const webdriver = require("selenium-webdriver")
 const { rsvp: url } = require("../../config.json")
 
 describe("RSVP", () => {
-  it("should have an invitee list", async () => {
-    const driver = await new webdriver.Builder().forBrowser("chrome").build()
+  let driver
+
+  beforeAll(async () => {
+    driver = new webdriver.Builder().forBrowser("chrome").build()
     await driver.get(url)
+  })
+  afterAll(async () => {
+    await driver.quit()
+  })
+
+  it("should have an invitee list", async () => {
     const elements = await driver.findElements(By.id("invitedList"))
     assert(elements.length > 0)
-    driver.quit()
+  })
+  it("should have a registration form", async () => {
+    const elements = await driver.findElements(By.id("registrar"))
+    assert(elements.length > 0)
   })
 })
