c0d4b3e42e6a1f8cf737405913d34c2c62971903
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-29 19:54:04 -0700

    Used explicit wait for elements to load on page.

--- rsvp/tests/invitees.test.js
+++ rsvp/tests/invitees.test.js
@@ -20,16 +20,24 @@ describe("RSVP", () => {
     const elements = await driver.findElements(page.locators.invitedList)
     assert(elements.length > 0)
   })
+
   it("should have a registration form", async () => {
     const elements = await driver.findElements(page.locators.registrationForm)
     //assert(elements.length > 0)
     expect(elements.length).toBeGreaterThan(0)
   })
+
   it("should load existing invitations", async () => {
     await driver.manage().setTimeouts({ implicit: 3000 })
     const elements = await driver.findElements(page.locators.invitees)
     const text = await elements[1].getText()
-    assert(text.includes("Craig Dennis"))
     expect(text).toMatch("Craig Dennis")
+  })
+
+  it("should load existing invitations", async () => {
+    const list = await driver.findElement(page.locators.invitedList)
+    await driver.wait(until.elementLocated(page.locators.invitees))
+    const text = await list.getText()
+    assert(text.includes("Craig Dennis"))
   })
 })
